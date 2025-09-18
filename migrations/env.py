73f33a0f863dbf96a8bb7b090e6d9aa4aa44de58
Igor Da Silva Santos <igor.ssantos@jsl.com.br>
2025-09-18 19:44:13 -0300

    Fix: Add project root to sys.path in env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add project root to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
 import logging
 from logging.config import fileConfig
 
